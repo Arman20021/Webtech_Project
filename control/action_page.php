25a8995c1ea83d23cedf3662ec26d2579b7550ea
Dima <saziaafrin711@gmail.com>
2025-05-27 14:56:30 +0600

    Admin page action page

--- control/action_page.php
+++ control/action_page.php
@@ -2,12 +2,11 @@
 include "../database/data.php";
 ?>
 <?php
-$fnameErr = $lnameErr = $usrnErr = $passErr = $confpassErr = $emailErr = $phErr = $DOBErr = $gdrErr = $pictureError = "";
-$fname = $lname = $usrn = $pass = $confpass = $email = $ph = $DOB = $gdr = $hasError= "";
-$outputMessage = "";
-
+$fnameErr = $lnameErr = $usrnErr = $passErr = $confpassErr = $emailErr = $phErr = $DOBErr = $gdrErr = $pictureError ="";
+$fname = $lname = $usrn = $pass = $confpass = $email = $ph = $DOB = $gdr = $fileName = "";
+$hasError = false;
 $errors = [];
- 
+$picturePath =  "";
 
 function test_input($data){
     $data = trim($data);
@@ -22,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["fname"])) {
         $fnameErr = "First name is required";
         $errors[] = $fnameErr;
-        $hasError=1;
+        $hasError = true;
     } else {
         $fname = test_input($_POST["fname"]);
     }
@@ -31,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["lname"])) {
         $lnameErr = "Last name is required";
         $errors[] = $lnameErr;
-         $hasError=1;
+        $hasError = true;
     } else {
         $lname = test_input($_POST["lname"]);
     }
@@ -40,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["usrname"])) {
         $usrnErr = "Username is required";
         $errors[] = $usrnErr;
-         $hasError=1;
+        $hasError = true;
     } else {
         $usrn = test_input($_POST["usrname"]);
     }
@@ -49,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["pass"])) {
         $passErr = "Password is required";
         $errors[] = $passErr;
-         $hasError=1;
+        $hasError = true;
     } else {
         $pass = test_input($_POST["pass"]);
     }
@@ -58,12 +57,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["confirmpass"])) {
         $confpassErr = "Confirm your password";
         $errors[] = $confpassErr;
-         $hasError=1;
+        $hasError = true;
     } else {
         $confpass = test_input($_POST["confirmpass"]);
         if ($confpass !== $pass) {
             $confpassErr = "Passwords do not match";
             $errors[] = $confpassErr;
+            $hasError = true;
         }
     }
 
@@ -71,7 +71,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["email"])) {
         $emailErr = "Email is required";
         $errors[] = $emailErr;
-         $hasError=1;
+        $hasError = true;
     } else {
         $email = test_input($_POST["email"]);
     }
@@ -80,7 +80,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["phnum"])) {
         $phErr = "Phone number is required";
         $errors[] = $phErr;
-         $hasError=1;
+        $hasError = true;
     } else {
         $ph = test_input($_POST["phnum"]);
     }
@@ -89,7 +89,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["DOB"])) {
         $DOBErr = "Date of birth is required";
         $errors[] = $DOBErr;
-         $hasError=1;
+        $hasError = true;
     } else {
         $DOB = test_input($_POST["DOB"]);
     }
@@ -98,7 +98,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["gender"])) {
         $gdrErr = "Gender is required";
         $errors[] = $gdrErr;
-         $hasError=1;
+        $hasError = true;
     } else {
         $gdr = test_input($_POST["gender"]);
     }
@@ -107,80 +107,55 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_FILES["myfile"]["name"])) {
         $pictureError = "Picture is required.";
         $errors[] = $pictureError;
-         $hasError=1;
+        $hasError = true;
     } else {
-        $fileName = $_FILES["myfile"]["name"];
+        $fileName = $usrn . "--" . basename($_FILES["myfile"]["name"]);
+        $picturePath = "../uploads/" . $fileName;
         $fileSize = $_FILES["myfile"]["size"];
-     
-     
 
-      if ($fileSize > 2 * 1024 * 1024) {
-            $pictureError = "File size must be less than 2MB.";
+        if ($fileSize > 2 * 1024 * 1024) {
+            $pictureError = "Picture must be less than 2MB.";
             $errors[] = $pictureError;
-        } 
-        else {
-          if(move_uploaded_file($_FILES["myfile"]["tmp_name"],"../uploads/".$_REQUEST["usrname"]."--".$_FILES["myfile"]["name"]))
-          {
-            echo "File Uploaded";
-          }
-            else {
-                $pictureError = "File upload failed.";
-                $errors[] = $pictureError;
-                 $hasError=1;
-            }
+            $hasError = true;
+        } elseif (!move_uploaded_file($_FILES["myfile"]["tmp_name"], $picturePath)) {
+            $pictureError = "Failed to upload picture.";
+            $errors[] = $pictureError;
+            $hasError = true;
         }
     }
 
-    // Final output
-if (!empty($errors)) {
-    $outputMessage .= "<div class='error-box'>";
+         
 
-    $outputMessage .= "<ol>";
-    foreach ($errors as $error) {
-        $outputMessage .= "<li>$error</li>";
+    // Display result
+    if (!empty($errors)) {
+        echo "<div style='color: red; text-align: left; width: 60%; margin: 0 auto;'>";
+        echo "<ol>";
+        foreach ($errors as $error) {
+            echo "<li>$error</li>";
+        }
+        echo "</ol></div><br>";
+    } else {
+        echo "<div style='width: 60%; margin: 0 auto; padding: 20px; background: #dff0d8; border: 1px solid #3c763d; color: #3c763d; border-radius: 5px;'>";
+        echo "<h2>Registration Successful!</h2>";
+        echo "<p><strong>First Name:</strong> $fname</p>";
+        echo "<p><strong>Last Name:</strong> $lname</p>";
+        echo "<p><strong>Username:</strong> $usrn</p>";
+        echo "<p><strong>Email:</strong> $email</p>";
+        echo "<p><strong>Phone:</strong> $ph</p>";
+        echo "<p><strong>Birth Date:</strong> $DOB</p>";
+        echo "<p><strong>Gender:</strong> $gdr</p>";
+        echo "<p><strong>Picture:</strong> $fileName</p>";
+        echo "</div><br>";
+
+        // Insert into database
+        $connobj = createCon();
+        if (insertData($connobj, $fname, $lname, $usrn, $pass, $confpass, $email, $ph, $DOB, $gdr, $picturePath)) {
+            echo "<p style='color: green;'>Data inserted successfully.</p>";
+            // header("Location: ../view/login.php");
+        } else {
+            echo "<p style='color: red;'>Failed to insert data.</p>";
+        }
+        closeConn($connobj);
     }
-    $outputMessage .= "</ol></div><br>";
-} else {
-   $outputMessage .= "<div class='success-box'>";
-
-    $outputMessage .= "<h2>Registration Successful!</h2>";
-    $outputMessage .= "<p><strong>First Name:</strong> $fname</p>";
-    $outputMessage .= "<p><strong>Last Name:</strong> $lname</p>";
-    $outputMessage .= "<p><strong>Username:</strong> $usrn</p>";
-    $outputMessage .= "<p><strong>Email:</strong> $email</p>";
-    $outputMessage .= "<p><strong>Phone:</strong> $ph</p>";
-    $outputMessage .= "<p><strong>Birth Date:</strong> $DOB</p>";
-    $outputMessage .= "<p><strong>Gender:</strong> $gdr</p>";
-    $outputMessage .= "<p><strong>Picture:</strong> $fileName</p>";
-    $outputMessage .= "</div><br>";
-}
-
-
-
-
-
-
-
-
-  $connobj=createCon();
-if($hasError=="")
-{
-  
-    if(insertData($connobj,$_POST["fname"],$_POST["lname"], 
-    $_POST["usrname"], $_POST["pass"],$_POST["confirmpass"],$_POST["email"]
-    ,$_POST["phnum"],$_POST["DOB"],$_POST["gender"],"../uploads/".$_REQUEST["usrname"]."--".$_FILES["myfile"]["name"]))
-    {
-        header("Location: ../view/login.php");
-
-    }
-    else 
-    {
-        echo"Failed";
-    }
-    closeConn($conobj);
-}
-
-
-
 }
 ?>
